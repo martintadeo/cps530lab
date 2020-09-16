77291d239060731f1f7dfe7da7813f9548e3aaf5
martintadeo <71407915+martintadeo@users.noreply.github.com>
2020-09-16 17:38:02 -0400

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,19 @@
-This a test
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<title> Admin</title>
+<meta charset="utf-8">
+</head>
+<body>
+<h1> Testing</h1>
+
+
+<p>Admin only
+</p>
+<blockquote>
+The power of the Web is in its universality. <i>Access by everyone</i> regardless of disability is an essential aspect
+</blockquote>
+
+
+</body>
+</html>
